19fc8101027e91355456763f4039e58466923e0a
Trang Hoang <tychuotsieu45@gmail.com>
2021-10-21 20:26:43 -0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-links');
 
-console.log(hamburger);
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-item').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
+
+
 
 
